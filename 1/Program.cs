5b2b0704f8c331369973ca1135cddedabb283186
aFFekT2212 <ksawier2212@gmail.com>
2022-08-16 12:18:52 +0300

    16.08

---
+++ 1/Program.cs
@@ -0,0 +1,7 @@
+﻿int m = Int32.Parse(Console.ReadLine());
+
+int count = 0;
+for (int i = 0; i < m; i++)
+    if (Int32.Parse(Console.ReadLine()) > 0) count ++;
+
+Console.WriteLine(count);
---
+++ 2/Program.cs
@@ -0,0 +1,10 @@
+﻿int b1 = int.Parse(Console.ReadLine());
+int k1 = int.Parse(Console.ReadLine());
+
+int b2 = int.Parse(Console.ReadLine());
+int k2 = int.Parse(Console.ReadLine());
+
+double x = (b1 - b2) / ((k2 - k1) * 1.0);
+double y = k2 * x + b2;
+Console.WriteLine(x);
+Console.WriteLine(y);
